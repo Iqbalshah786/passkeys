5832e91c1e9248d778fcf97b9f6ff7d22ec4dcfa
Iqbal Shah <iqbalshahnadiri786@gmail.com>
2025-04-12 18:21:49 +0500

    update investment card ui

--- lib/widgets/investment_card_widget.dart
+++ lib/widgets/investment_card_widget.dart
@@ -9,54 +9,57 @@ class InvestmentCardWidget extends StatelessWidget {
     double screenHeight = MediaQuery.of(context).size.height;
 
     return Container(
-
-      padding: EdgeInsets.symmetric(vertical:  12.h , horizontal:  12.w ),
-      width: screenHeight < 700 ? 350.w : 360.w,
-      height: 80.h,
-
+      padding: EdgeInsets.all(12.w),
       //border color
       decoration: BoxDecoration(
         color: Color(0xfff4efff),
         border: Border.all(
           color: Color(0xff5F09FF),
-          width: 0.5.w,
+            width: 0.5.w,
         ),
         borderRadius: BorderRadius.circular(8.w),
 
       ),
 
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Image.asset('assets/icons/crown.png',width:screenHeight < 700 ? 55.w : 60.w ,height: screenHeight < 700 ? 55.h : 60.h,),
-          SizedBox(width: 5.w),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Start investing now!',
-                  style: TextStyle(
-                    fontSize: 14.sp,
-                    fontWeight: FontWeight.w600,
-                    color: Color(0xff5F09FF),
+          Row(
+            children: [
+              Row(
+                children: [
+                  Image.asset('assets/icons/crown.png',width: 46.w,height: 46.h,),
+                ],
+              ),
+              SizedBox(width: 5.w),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Start investing now!',
+                    style: TextStyle(
+                      fontSize: 14.sp,
+                      fontWeight: FontWeight.w600,
+                      color: Color(0xff5F09FF),
+                    ),
                   ),
-                ),
-                SizedBox(height: 2.h),
-                Text(
-                  'Protected savings and investment plans.',
-                  style: TextStyle(
-                    fontSize:  10.5.sp,
-                    fontWeight: FontWeight.w400,
-                    color: Color(0xff121212),
+                  Text(
+                    'Protected savings and investment plans.',
+                    style: TextStyle(
+                      fontSize:  10.sp,
+                      fontWeight: FontWeight.w400,
+                      color: Color(0xff121212),
+                    ),
                   ),
-                ),
-              ],
-            ),
+                ],
+              ),
+            ],
           ),
           Container(
-            child: Image.asset('assets/icons/ai.png',width: 37.w,height: 24.h,),
-            ),
+            child: Image.asset('assets/icons/ai.png',width: 27.w,height: 14.h,),
+          ),
         ],
       ),
     );
